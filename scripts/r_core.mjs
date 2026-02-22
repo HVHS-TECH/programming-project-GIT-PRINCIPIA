4125d5f8d39bac3a6ab992b9f0c415947677904c
= <=>
2026-02-23 11:05:39 +1300

    Added core scripts, began on implementation.

---
+++ scripts/r_core.mjs
@@ -0,0 +1,110 @@
+//----------------------------------------------------------------------//
+//                         ---Astro Explorer---                         //
+//----------------------------------------------------------------------//
+//Written by Alex Curwen                                                //
+//Renderer core script:                                                 //
+//Manages drawing and other core renderer functions                     //
+//----------------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------------//
+//                            ---Exports---                             //
+//----------------------------------------------------------------------//
+
+export {r_core_initialize}
+
+//----------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------//
+//                            ---Imports---                             //
+//----------------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------//
+//Variables and constants//
+
+//Canvas
+var cnv; //The canvas object
+var cnv_width; //The canvas width
+var cnv_height; //The canvas height
+
+//Framebuffer
+var fbo; //The framebuffer object
+var fbo_width; //The framebuffer width
+var fbo_height; //The framebuffer height
+const FBO_SCALE = 2000; //The scale of the framebuffer. Only the height of the fbo changes on window resize, not the width. This width is instead set to FBO_SCALE
+
+//Renderer state
+var initialized = false;
+//----------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------//
+//Functions
+//----------------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------------//
+//r_core_initialize()                    //
+//called in g_startup.mjs on p5 startup()//
+//manages update logic                   //
+function r_core_initialize() {
+    //Initialize the canvas
+    cnv_width = windowWidth;
+    cnv_height = windowHeight - 100; //-100 as a small padding
+    cnv = createCanvas(cnv_width, cnv_height, WEBGL);
+
+    //Initialize the framebuffer
+    fbo_width = FBO_SCALE;
+    fbo_height = FBO_SCALE * windowHeight / windowWidth;
+    fbo = createFramebuffer();
+    fbo.resize(fbo_width, fbo_height);
+
+    //Update the renderer state
+    initialized = true; 
+}
+//----------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------//
+//Helper functions
+//----------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+//----------------------------------------------------------------------//
+//Callback functions
+//----------------------------------------------------------------------//
+
+
+//----------------------------------------------------------------------//
+//windowResize()
+//resizes the canvas to match the screen size
+//resizes the framebuffer to match the screen aspect ration
+function windowResized() {
+    if (!initialized) return; //Only resize the canvas and fbo if the fbo and canvas exist
+}
+//----------------------------------------------------------------------//
+
+
