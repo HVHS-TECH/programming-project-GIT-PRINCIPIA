f9507d23aca9395b8b0e7e90c6eef8780b73c7d2
PRINCIPIA <AlexMindstorms@hotmail.com>
2026-02-23 19:29:25 +0000

    Added page array, stopped using p5

--- scripts/r_core.mjs
+++ scripts/r_core.mjs
@@ -13,6 +13,7 @@
 //----------------------------------------------------------------------//
 
 export {r_core_initialize}
+export {r_core_setHasCnv}
 
 //----------------------------------------------------------------------//
 
@@ -31,17 +32,13 @@ export {r_core_initialize}
 
 //Canvas
 var cnv; //The canvas object
-var cnv_width; //The canvas width
-var cnv_height; //The canvas height
-
-//Framebuffer
-var fbo; //The framebuffer object
-var fbo_width; //The framebuffer width
-var fbo_height; //The framebuffer height
-const FBO_SCALE = 2000; //The scale of the framebuffer. Only the height of the fbo changes on window resize, not the width. This width is instead set to FBO_SCALE
+var canvas; //The canvas context
+var cnvWidth; //The canvas width
+var cnvHeight; //The canvas height
 
 //Renderer state
 var initialized = false;
+var hasCnv = false;
 //----------------------------------------------------------------------//
 
 
@@ -53,20 +50,21 @@ var initialized = false;
 
 //----------------------------------------------------------------------//
 //r_core_initialize()                    //
-//called in g_startup.mjs on p5 startup()//
-//manages update logic                   //
+//called in g_startup.mjs on startup()   //
+//initializes the renderer               //
 function r_core_initialize() {
-    //Initialize the canvas
-    cnv_width = windowWidth;
-    cnv_height = windowHeight - 100; //-100 as a small padding
-    cnv = createCanvas(cnv_width, cnv_height, WEBGL);
+    if (hasCnv) {
+        //Initialize the canvas
+        cnvWidth = window.innerWidth;
+        cnvHeight = window.innerHeight - 100; //-100 as a small padding
+        canvas = document.getElementById("canvas");
+        cnv = canvas.getContext("2d");
+        cnv.width = cnvWidth;
+        cnv.height = cnvHeight;
 
-    //Initialize the framebuffer
-    fbo_width = FBO_SCALE;
-    fbo_height = FBO_SCALE * cnv_height / cnv_width;
-    fbo = createFramebuffer();
-    fbo.resize(fbo_width, fbo_height);
-
+        //Set the callbacks
+        window.addEventListener('resize', cb_windowResized);
+    }
     //Update the renderer state
     initialized = true; 
 }
@@ -74,7 +72,14 @@ function r_core_initialize() {
 
 
 
-
+//----------------------------------------------------------------------//
+//r_core_setHasCanvas(_hasCnv)                       //
+//called in g_core.mjs setState from g_startup.mjs  //
+//sets whether or not to initialize the canvas      //
+function r_core_setHasCnv(_hasCnv) {
+    hasCnv = _hasCnv;
+}
+//----------------------------------------------------------------------//
 
 
 
@@ -102,19 +107,17 @@ function r_core_initialize() {
 //windowResize()
 //resizes the canvas to match the screen size
 //resizes the framebuffer to match the screen aspect ration
-function windowResized() {
+function cb_windowResized() {
     if (!initialized) return; //Only resize the canvas and fbo if the fbo and canvas exist
 
     //Calculate the new canvas dimensions
-    cnv_width = windowWidth;
-    cnv_height = windowHeight;
+    cnv_width = window.innerWidth;
+    cnv_height = window.innerHeight - 100; //-100 as a small padding
 
     //Resize the canvas
-    cnv.resize(cnv_width, cnv_height);
+    cnv.width = cnv_width;
+    cnv.height = cnv_height;
 
-    //Calculate the new fbo dimensions
-    fbo_height = FBO_SCALE * cnv_height / cnv_width;
-    fbo.resize(fbo_width, fbo_height);
 }
 //----------------------------------------------------------------------//
 
